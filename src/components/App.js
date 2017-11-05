9b5d2d5491fcd69b868097afbe022c2db2242882
Masaori <masaori.hirono@gmail.com>
2017-11-05 18:30:01 +0900

    一通り実装

--- src/components/App.js
+++ src/components/App.js
@@ -2,54 +2,110 @@ import React from 'react';
 import Button from "material-ui/Button";
 import Card from "material-ui/Card";
 import Grid from "material-ui/Grid";
+import ScrollableAnchor from 'react-scrollable-anchor'
+import { ThumbUp } from 'material-ui-icons';
 
 import './app.scss';
 
 const logoImage = require('../images/logo.png');
+const batteryLowImage = require('../images/battery_low.png');
 
-// "Shibiya" Photo by Jason Ortego on Unsplash
+// "Shibuya" Photo by Jason Ortego on Unsplash
+// "Shibuya night" Photo by Redd Angelo on Unsplash
 
 class AppComponent extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <div id="content-title" className="title-content">
-          <div className="card">
-            <Grid container spacing={0}>
-              <Grid item xs={12} sm={3}>
-                <div className="card-content">
-                  <img
-                    src={logoImage}
-                    alt="2min battery Logo Image"
-                    className="logo-image"
-                  />
-                </div>
-              </Grid>
-              <Grid item xs={12} sm={9}>
-                <div className="card-content">
-                  <div className="card-title">
-                    2分でバッテリー
+      <div className="page-container">
+        <ScrollableAnchor id="content-title" className="anchor">
+          <div className="title-content">
+            <div className="card">
+              <div className="spacer" />
+              <Grid container spacing={0}>
+                <Grid item xs={12} sm={3}>
+                  <div className="card-content">
+                    <img
+                      src={logoImage}
+                      alt="2min battery Logo Image"
+                      className="logo-image"
+                    />
                   </div>
-                  <div className="card-subtitle">
-                    スマホのバッテリーがない！<br />
-                    救済サービス
+                </Grid>
+                <Grid item xs={12} sm={9}>
+                  <div className="card-content">
+                    <div className="card-title">
+                      2分でバッテリー
+                    </div>
+                    <div className="card-subtitle">
+                      スマホのバッテリーがない！<br />
+                      救済サービス
+                    </div>
                   </div>
-                </div>
+                </Grid>
               </Grid>
-            </Grid>
-            <div  className="spacer" />
-            <Button>
-              NEXT
-            </Button>
+              <div className="spacer" />
+              <Button
+                raised
+                color="primary"
+                href="#content-description"
+              >
+                くわしく
+              </Button>
+              <div className="spacer" />
+            </div>
           </div>
-        </div>
-        <div id="content-primary" className="description-content">
-
-        </div>
-        <div id="content-primary" className="registration-content">
-
-        </div>
+        </ScrollableAnchor>
+        <ScrollableAnchor id="content-description">
+          <div className="description-content">
+            <img
+              src={batteryLowImage}
+              alt="battery emergency"
+              className="battery-low-image"
+            />
+            <div className="card">
+              <div className="card-title">
+                「あっ、スマホのバッテリーが...！」
+              </div>
+              <div className="card-description">
+                外出先で突然訪れる、<br />
+                <div className="card-list">
+                  「あれ、スマホのバッテリーがない！」<br />
+                  「充電するのを忘れてた！」
+                </div>
+                こんなピンチを<b>「２分」</b>で解決するサービスが、あなたの街ではじまります。
+              </div>
+              <div className="spacer" />
+              <Button
+                raised
+                color="primary"
+                href="#content-registration"
+              >
+                次へ
+              </Button>
+            </div>
+          </div>
+        </ScrollableAnchor>
+        <ScrollableAnchor id="content-registration">
+          <div className="registration-content">
+            <ThumbUp className="thumb-up-image"/>
+            <div className="card">
+              <div className="card-description">
+                このサービスに興味はありませんか？<br />
+                わたしたちのFacebookページで、このサービスの最新情報をお届けします。
+                <div className="spacer" />
+              </div>
+              <div className="spacer" />
+              <Button
+                raised
+                color="primary"
+                href="https://www.facebook.com/2min.battery/"
+              >
+                facebookページでいいね！する
+              </Button>
+            </div>
+          </div>
+        </ScrollableAnchor>
       </div>
     );
   }
